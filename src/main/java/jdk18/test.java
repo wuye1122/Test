1e2ac638389e32d6f38c57de81e6b84b28621ff8
wuye1122 <15776469680@163.com>
2020-01-14 15:45:07 +0800

    develop-06 test.java

---
+++ src/main/java/jdk18/test.java
@@ -0,0 +1,29 @@
+package jdk18;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.net.URI;
+
+public class test {
+
+    public static boolean isSuccess(int a){
+        try{
+            Integer status = a;
+            if(null!=status&&(status >= 200 && status < 300)){
+                return true;
+            }else{
+                throw  new Exception("this is a success access!");
+            }
+        }catch (Exception e){
+            System.out.println("e:"+e.getMessage());
+            return false;
+        }
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("boolean:"+isSuccess(20));
+
+        // request 2
+    }
+}
