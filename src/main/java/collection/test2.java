1405fdb9d1956e7e612dc3951fcbddf074ee3546
wuye1122 <15776469680@163.com>
2020-02-06 15:38:10 +0800

    cherry - pick

--- src/main/java/collection/test2.java
+++ src/main/java/collection/test2.java
@@ -4,6 +4,9 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("this is the first commit");
 
+
+        System.out.println("this is the last commit");
+
     }
 
 }
--- src/main/java/collection/test3.java
+++ src/main/java/collection/test3.java
@@ -6,6 +6,6 @@ public class test3 {
     public static void main(String[] args) {
         System.out.println(new test2());
 
-        System.out.println(new test3());
+        System.out.println(new test3()+"this a good time");
     }
 }
