a04d0292868b037f6579585432b0426e567551e7
wuye1122 <15776469680@163.com>
2020-02-06 15:40:35 +0800

    4 added 6 modified

--- src/main/java/collection/test3.java
+++ src/main/java/collection/test3.java
@@ -4,7 +4,9 @@ public class test3 {
 
 
     public static void main(String[] args) {
-        System.out.println();
         System.out.println(new test2());
+
+        System.out.println(new test3()+"this a good time");
+        System.out.println("this is a test3");
     }
 }
