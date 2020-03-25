258a37a741bfdb71023b4ef2bfdaee4f5bee2746
wuye1122 <15776469680@163.com>
2020-03-25 17:16:24 +0800

    #444

--- src/main/java/collection/test3.java
+++ src/main/java/collection/test3.java
@@ -9,5 +9,6 @@ public class test3 {
         System.out.println("1111--222");
         System.out.println(new test3()+"this a good time");
         System.out.println("this is a test3");
+        System.out.println("3333---444");
     }
 }
