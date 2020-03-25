1636c54f8b9bf8fa913e659c8620589477e51500
wuye1122 <15776469680@163.com>
2020-03-25 17:15:56 +0800

    #33

--- src/main/java/collection/test3.java
+++ src/main/java/collection/test3.java
@@ -6,6 +6,7 @@ public class test3 {
     public static void main(String[] args) {
         System.out.println(new test2());
 
+        System.out.println("1111--222");
         System.out.println(new test3()+"this a good time");
         System.out.println("this is a test3");
     }
